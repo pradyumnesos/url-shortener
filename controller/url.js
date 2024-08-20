f1158f83870a6572fe4a6596840dabbbc93c9497
pradyumnesos <pradyumnaojha856@gmail.com>
2024-08-20 13:31:25 +0530

    added url validation

--- controller/url.js
+++ controller/url.js
@@ -1,38 +1,59 @@
 const shortid = require('shortid');
-const URL = require('../models/url')
+const Url = require('../models/url')
+const {URL} = require('url');
+function validateUrl(urlString) {
+  try {
+    new URL(urlString); 
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
 async function handleGenerateNewShortURL(req,res){
   const body = req.body;
+  try{
   if(!body.url) return res.status(400).json({error: "url is required"})
-
+  if(!validateUrl(body.url)) return res.status(400).send({ error : "Invalid url" } )  
   const shortID = shortid();
   
-  const result = await URL.findOne({redirectURL:body.url});
+  const result = await Url.findOne({redirectURL:body.url});
   if(result) return res.status(400).json({error: "url is used"})
-  await URL.create({
+  await Url.create({
     shortId: shortID,
     redirectURL: body.url,
     visitHistory:[],
   });
-  const urls = await URL.find({});
+  const urls = await Url.find({});
   const protocol = req.protocol;
   const host = req.hostname;
-  const Url = req.originalUrl;
   const port = process.env.PORT
   return res.render("home",{id: shortID,urls,protocol,host,port});
-
+  }catch(e){
+  console.log(e)
+  res.status(500).send({ error : "Internal Server Error" } )
+  }
 }
 
 async function handleGetAnalytics(req,res){
   const shortId = req.params.shortId;
-  const result = await URL.findOne({shortId});
+  try{
+
+  
+  const result = await Url.findOne({shortId});
   res.json({
     clicks: result.visitHistory.length,
     
-    analytics:result.visitHistory}); 
+    analytics:result.visitHistory});
+  }catch(e){
+    console.log(e)
+    res.status(500).send({ error : "Internal Server Error" } )
+  }   
 }
 async function handleRedirects(req,res){
   const shortId = req.params.shortId;
-  const entry = await URL.findOneAndUpdate({shortId},{
+  try{
+  const entry = await Url.findOneAndUpdate({shortId},{
       $push:{
           visitHistory:{
               timestamp:Date.now(),
@@ -40,15 +61,23 @@ async function handleRedirects(req,res){
       }
   });
   res.redirect(entry.redirectURL);
+  }catch(e){
+    console.log(e)
+    res.status(500).send({ error : "Internal Server Error" } )
+  }
 }
 async function handleStaticRoute(req,res){
+  try{
   const protocol = req.protocol;
   const host = req.hostname;
-  const Url = req.originalUrl;
   const port = process.env.PORT;
-  const urls = await URL.find({});
+  const urls = await Url.find({});
   res.render('home',{
     urls,protocol,host,port
    })
+  }catch(e){
+    console.log(e)
+    res.status(500).send({ error : "Internal Server Error" } )
+  }
 }
 module.exports = {handleGenerateNewShortURL,handleGetAnalytics,handleRedirects,handleStaticRoute}
